afdb894264f0166f5a759b3d6f6d70893bf918eb
kynel <kynel95@gmail.com>
2021-07-19 17:10:31 +0900

    typescript with html form, input event
    typescript with html form, input event

--- react_with_typescript/src/App.tsx
+++ react_with_typescript/src/App.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import { Form, Input } from "./Input";
+import Number from "./Number";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  counter: number;
+  name: string;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0,
+    name: "",
+  };
+  render() {
+    const { counter, name } = this.state;
+    return (
+      <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
+        <Number count={counter}>{counter}</Number>
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  add = (): void => {
+    this.setState((prev) => {
+      return {
+        counter: prev.counter + 1,
+      };
+    });
+  };
 }
 
 export default App;
